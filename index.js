eb572c08be14b4d0194dba03bdc69a140ca51ebc
devy <60780448+SK-Fast@users.noreply.github.com>
2023-11-27 23:30:45 +0700

    change to http-proxy

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
-const express = require('express')
-const axios = require("axios").default
-const cookieParser = require('cookie-parser')
-const app = express()
-const port = process.env.PORT || 3000
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
 
-app.use(express.text({ type: "*/*" }))
+const http = require('https'),
+    httpProxy = require('http-proxy');
 
-app.all('*', async (req, res) => {
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer(function (req, res) {
+    proxy.web(req, res, {
+        target: 'https://rayongwit.ac.th/',
+        secure: true,
+        ws: false,
+        prependPath: false,
+        ignorePath: false,
+        changeOrigin: true,
+        auth: false
+    })
+})
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
     res.setHeader("Access-Control-Allow-Origin", req.headers.origin ?? "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "*");
-
-    console.log("SERVING PROXY")
-
-    let databack = ""
-
-    try {
-        const originRes = await axios.request({
-            url: `https://rayongwit.ac.th${req.path}`,
-            method: req.method,
-            data: req.body,
-            headers: {
-                "Cookie": req.headers["cookie"] ?? "",
-                "Content-Type": req.headers["content-type"] ?? ""
-            }
-        })
-        
-        databack = originRes.data
-    
-        const data = originRes.data
-        let rywlcommands = ""
-    
-        if (originRes.headers["set-cookie"]) {
-            res.setHeader("Set-Cookie", `${originRes.headers["set-cookie"]}; SameSite=None; Secure`)
-        }
-    
-        res.send(`${rywlcommands}${data}`)
-    } catch (err) {
-        res.status(502)
-        res.send(databack ?? "ERR")
-        console.log("FAILURE: ", databack)
-        console.log(err)
-    }
 })
 
-app.listen(port, () => {
-    console.log(`RYWProxy listening at http://localhost:${port}`)
-})
+console.log("Listening")
+server.listen(port)
