b56653f8e5a75b717277580bce4fac937eb00e2f
devy <60780448+SK-Fast@users.noreply.github.com>
2023-11-27 20:04:48 +0700

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const axios = require("axios").default
+const cookieParser = require('cookie-parser')
+const app = express()
+const port = 3000
+
+app.use(express.text({ type: "*/*" }))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+
+    next();
+});
+
+app.all('*', async (req, res) => {
+    console.log(req.method)
+    console.log(req.headers)
+    console.log(req.body)
+    console.log(req.headers.cookie)
+
+    const originRes = await axios.request({
+        url: `https://rayongwit.ac.th${req.path}`,
+        method: req.method,
+        data: req.body,
+        headers: {
+            "Cookie": req.headers["cookie"] ?? "",
+            "Content-Type": req.headers["content-type"] ?? ""
+        }
+    })
+
+    const data = originRes.data
+    let rywlcommands = ""
+
+    if (originRes.headers["set-cookie"]) {
+        res.setHeader("Set-Cookie", `${originRes.headers["set-cookie"]}; SameSite=None; Secure`)
+    }
+
+    res.send(`${rywlcommands}${data}`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
