d7466c7bc296c374020bf54e5238a4530438f513
devy <60780448+SK-Fast@users.noreply.github.com>
2023-11-28 00:16:22 +0700

    def real browser

--- index.ts
+++ index.ts
@@ -19,8 +19,24 @@ app.all('*', async (req, res) => {
             method: req.method,
             data: req.body,
             headers: {
-                "Cookie": req.headers["cookie"] ?? "",
-                "Content-Type": req.headers["content-type"] ?? ""
+                "Cookie": req.headers["cookie"] ?? "my_lang=th",
+                "Content-Type": req.headers["content-type"] ?? "",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9,th-TH;q=0.8,th;q=0.7,zh-CN;q=0.6,zh;q=0.5",
+                "Cache-Control": "max-age=0",
+                "Connection": "keep-alive",
+                "Dnt": "1",
+                "Host": "rayongwit.ac.th",
+                "Sec-Ch-Ua": '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+                "Sec-Ch-Ua-Mobile": '?0',
+                "Sec-Ch-Ua-Platform": '"Windows"',
+                "Sec-Fetch-Dest:": 'document',
+                "Sec-Fetch-Mode": 'navigate',
+                "Sec-Fetch-Site": 'none',
+                "Sec-Fetch-User": '?1',
+                "Upgrade-Insecure-Requests": '1',
+                "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
             }
         })
 
